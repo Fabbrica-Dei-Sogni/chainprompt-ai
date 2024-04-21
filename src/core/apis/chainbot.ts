eaedd1eaca502bd4f8145e1ec41387619d1445bc
Alessandro Modica <alessandro.modica@gmail.com>
2024-04-21 10:17:55 +0000

    consolidato il layer service di langchain, implementato il dinamismo di esposizione degli endpoint a partire dai prompt tematici , allo stesso identico modo della chatbot precedente.
    definiti i baseuri per modelli llm caricati tramite llmstudio (locale sandbox) da quelli caricati su un server ollama (locale produzione)
    
    Eseguiti test sulle performance e con maggior dettaglio sulle casistiche di timeout.
    
    gli endpoint delle chatbot tramite request sono rimasti attivi e lo rimarranno per questione di retrocompatibilità legacy

--- src/core/apis/chainbot.ts
+++ src/core/apis/chainbot.ts
@@ -3,24 +3,114 @@
 // In modo speculare, le stesse chiamate sono definite nel protocollo socket.io.
 import express from "express";
 const router = express.Router();
-import { getAnswerLLM, getAnswerLocalLLM } from '../services/langchainservice.js';
-import { ContextChat } from '../services/commonservices.js';
+import { getAnswerLLM, getAnswerLocalLLM, getAnswerOllamaLLM } from '../services/langchainservice.js';
+import { writeObjectToFile, contextFolder, ContextChat } from '../services/commonservices.js';
 import { getFrameworkPrompts } from '../services/builderpromptservice.js';
+import * as requestIp from 'request-ip';
+import fs from 'fs';
 
+const conversations: Record<string, any> = {};
+const contexts = fs.readdirSync(contextFolder);
 
-router.post(`/localai/prompt/test`, async (req: any, res: any, next: any) => {
+// Funzione per gestire la richiesta del prompt per un determinato contesto
+const handleLocalRequest = async (req: any, res: any, next: any) => {
     try {
-        const systemPrompt = await getFrameworkPrompts(ContextChat.CHAT_BOT_CV);
-
-        const question = '\n' + req.body.question;
-        const modelname = req.body.modelname;
-        const temperature = req.body.temperature;
-        let answer = await getAnswerLocalLLM(systemPrompt, question, temperature, modelname);
+        const originalUriTokens = req.originalUrl.split('/');
+        const context = originalUriTokens[originalUriTokens.length - 1];
+        const systemPrompt = await getFrameworkPrompts(context); // Ottieni il prompt di sistema per il contesto
+        let answer = await getAndSendPromptLocalLLM(req, res, systemPrompt, context); // Invia il prompt al client
         res.json({ answer }); // Invia la risposta al client
     } catch (err) {
         console.error('Errore durante la conversazione:', err);
         res.status(500).json({ error: `Si è verificato un errore interno del server` });
     }
+};
+
+const handleCloudLLMRequest = async (req: any, res: any, next: any) => {
+    try {
+        const originalUriTokens = req.originalUrl.split('/');
+        const context = originalUriTokens[originalUriTokens.length - 1];
+        const systemPrompt = await getFrameworkPrompts(context); // Ottieni il prompt di sistema per il contesto
+        let answer = await getAndSendPromptCloudLLM(req, res, systemPrompt, context); // Invia il prompt al client
+        res.json({ answer }); // Invia la risposta al client
+    } catch (err) {
+        console.error('Errore durante la conversazione:', err);
+        res.status(500).json({ error: `Si è verificato un errore interno del server` });
+    }
+};
+
+const handleLocalOllamaRequest = async (req: any, res: any, next: any) => {
+    try {
+        const originalUriTokens = req.originalUrl.split('/');
+        const context = originalUriTokens[originalUriTokens.length - 1];
+        const systemPrompt = await getFrameworkPrompts(context); // Ottieni il prompt di sistema per il contesto
+        let answer = await getAndSendPromptbyOllamaLLM(req, res, systemPrompt, context); // Invia il prompt al client
+        res.json({ answer }); // Invia la risposta al client
+    } catch (err) {
+        console.error('Errore durante la conversazione:', err);
+        res.status(500).json({ error: `Si è verificato un errore interno del server` });
+    }
+};
+
+
+
+async function getAndSendPromptCloudLLM(req: any, res: any, systemPrompt: string, contextchat: string) {
+    return await callBackgetAndSendPromptbyLocalRest(req, res, systemPrompt, contextchat, getAnswerLLM);
+}
+
+async function getAndSendPromptLocalLLM(req: any, res: any, systemPrompt: string, contextchat: string) {
+    return await callBackgetAndSendPromptbyLocalRest(req, res, systemPrompt, contextchat, getAnswerLocalLLM);
+}
+
+async function getAndSendPromptbyOllamaLLM(req: any, res: any, systemPrompt: string, contextchat: string) {
+    return await callBackgetAndSendPromptbyLocalRest(req, res, systemPrompt, contextchat, getAnswerOllamaLLM);
+}
+
+async function callBackgetAndSendPromptbyLocalRest(req: any, res: any, systemPrompt: string, contextchat: string, callbackRequestLLM: any) {
+    const { systemprompt, question, temperature, modelname, keyconversation } = buildAndTrackPromptRest(req, systemPrompt, contextchat);
+    const assistantResponse = await callbackRequestLLM(systemprompt, question, temperature || 0.1, modelname);
+    conversations[keyconversation].conversationContext += `\nAI: ${assistantResponse}\n`;
+    await writeObjectToFile(conversations, contextchat);
+
+    return assistantResponse;
+}
+
+function buildAndTrackPromptRest(req: any, systemPrompt: string, context: string) {
+    console.log("Estrazione informazioni data input per la preparazione al prompt di sistema....");
+
+    const question = '\n' + req.body.question;
+    const modelname = req.body.modelname;
+    const temperature = req.body.temperature;
+    const ipAddress = requestIp.getClientIp(req);
+    const keyconversation = ipAddress + "_" + context;
+
+    // Crea una nuova conversazione per questo indirizzo IP
+    if (!conversations[keyconversation]) {
+        conversations[keyconversation] = {
+            startTime: new Date(),
+            conversationContext: systemPrompt,
+        };
+    }
+    conversations[keyconversation].conversationContext += `\nHuman: ${question}\n`;
+    console.log("Indirizzo ip: ", ipAddress);
+    console.log("Domanda ricevuta:", question);
+    const systemprompt = conversations[keyconversation].conversationContext;
+
+    return { systemprompt, question, temperature, modelname, keyconversation };
+}
+
+// Genera le route dinamicamente per ogni contesto disponibile
+contexts.forEach(context => {
+    router.post(`/langchain/localai/prompt/${context}`, handleLocalRequest);
+});
+
+// Genera le route dinamicamente per ogni contesto disponibile
+contexts.forEach(context => {
+    router.post(`/langchain/cloud/prompt/${context}`, handleCloudLLMRequest);
+});
+
+contexts.forEach(context => {
+    router.post(`/langchain/ollama/prompt/${context}`, handleLocalOllamaRequest);
 });
 
 export default router;
--- src/core/services/langchainservice.ts
+++ src/core/services/langchainservice.ts
@@ -1,10 +1,13 @@
 import { PromptTemplate } from "@langchain/core/prompts";
 import { OpenAI } from "@langchain/openai";
+import { Ollama } from "@langchain/community/llms/ollama";
 import { LLMChain } from "langchain/chains";
 import dotenv from "dotenv";
 dotenv.config();
 
-const urilocalai = process.env.URI_LOCALAI || 'http://eleanor:1234/v1/chat/completions'; // Usa il valore della variabile di ambiente PORT, se definita, altrimenti usa la porta 3000
+const urilocalai = process.env.URI_LANGCHAIN_LLMSTUDIO || 'http://eleanor:1234/v1'; // Usa il valore della variabile di ambiente PORT, se definita, altrimenti usa la porta 3000
+const uriollama = process.env.URI_LANGCHAIN_OLLAMA || 'http://kali:1234'; // Usa il valore della variabile di ambiente PORT, se definita, altrimenti usa la porta 3000
+
 const local_model_name = process.env.LOCAL_MODEL_NAME || "gpt-4-turbo";
 // Dizionario che mappa gli indirizzi IP alle conversazioni
 const conversations: Record<string, any> = {};
@@ -15,10 +18,9 @@ const getAnswerLLM = async (systemprompt: string, question: string, temperature:
     const llmChain = new LLMChain({
 
         llm: new OpenAI({ apiKey: process.env.OPENAI_API_KEY, temperature, modelName }),
-        prompt: PromptTemplate.fromTemplate(systemprompt),
+        prompt: PromptTemplate.fromTemplate(systemprompt + " " + question),
     });
-    const response = await llmChain.call(question);
-    const answer = response.choices[0].message.content.trim();
+    const answer = await llmChain.invoke({ topic: question });
     console.log("Risposta generata:", answer);
     return answer;
 }
@@ -31,14 +33,31 @@ const getAnswerLocalLLM = async (systemprompt: string, question: string, tempera
             configuration: {
                 baseURL: urilocalai,
             },
-            temperature, modelName
+            temperature, modelName,
         }),
-        prompt: PromptTemplate.fromTemplate(systemprompt),
+        prompt: PromptTemplate.fromTemplate(systemprompt + " " + question),
     });
-    const answer = await llmChain.call(question);
+    const answer = await llmChain.invoke({ topic: question });
+    console.log("Risposta generata:", answer);
+    return answer;
+}
+
+const getAnswerOllamaLLM = async (systemprompt: string, question: string, temperature: number, modelName: string = local_model_name) => {
+
+    const llmChain = new LLMChain({
+        llm: new Ollama({
+            baseUrl: uriollama,
+            temperature,
+            model: modelName,
+            numCtx: 8032,
+            keepAlive: "24h",
+        }),
+        prompt: PromptTemplate.fromTemplate(systemprompt + " " + question),
+    });
+    const answer = await llmChain.invoke({ topic: question });
     console.log("Risposta generata:", answer);
     return answer;
 }
 
 
-export { getAnswerLLM, getAnswerLocalLLM };
+export { getAnswerLLM, getAnswerLocalLLM, getAnswerOllamaLLM };
