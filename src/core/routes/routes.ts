20e5b98c28f180b1c751c90cf29af7e93d96c0e6
Alessandro Modica <alessandro.modica@gmail.com>
2024-09-07 16:45:11 +0000

    rinominato in classicbot le api classiche di accesso a llm

--- src/core/routes/routes.ts
+++ src/core/routes/routes.ts
@@ -1,6 +1,6 @@
 // Rappresenta il file in cui raggruppare le varie rotte definite nella cartella "apis"
 import express from "express";
-import chatbot from "../apis/chatbot.js"
+import classicbot from "../apis/classicbot.js"
 import chainbot from "../apis/chainbot.js"
 const router = express.Router();
 
@@ -9,7 +9,7 @@ const router = express.Router();
  */
 
 // Utilizza le rotte definite nel modulo "chatbot"
-router.use(chatbot);
+router.use(classicbot);
 router.use(chainbot);
 
 console.log(`Importazione delle API avvenuta con successo!`);
