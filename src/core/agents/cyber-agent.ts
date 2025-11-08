dc470926d87a56d414ce699b64610db925b2b49e
Alessandro Modica <alessandro.modica@gmail.com>
2025-11-08 18:52:56 +0000

    reaname variable

--- src/core/agents/cyber-agent.ts
+++ src/core/agents/cyber-agent.ts
@@ -4,10 +4,10 @@ import { CybersecurityAPITool } from "../tools/cybersecurityapi.tool.js";
 import { getAgent, invokeAgent } from "../services/agent.service.js";
 
 
-export async function getAnswerByThreatIntel(inputData: DataRequest, context: string, provider: LLMProvider) { 
+export async function getAnswerByThreatIntel(inputData: DataRequest, systemPrompt: string, provider: LLMProvider) { 
     const { question }: DataRequest = inputData;
 
-    const agent = await getAgent(inputData, provider, context, [new CybersecurityAPITool()]);
+    const agent = await getAgent(inputData, provider, systemPrompt, [new CybersecurityAPITool()]);
     const result = await invokeAgent(agent, question!);
 
     return result;
